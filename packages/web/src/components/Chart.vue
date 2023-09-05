11956cb77141bdeb1114d5b8491d3da5ce5b896c
Plumbiu <3434909403@qq.com>
2023-09-05 20:07:33 +0800

    fix(web): impove web part

--- packages/web/src/components/Chart.vue
+++ packages/web/src/components/Chart.vue
@@ -4,7 +4,7 @@ import echarts from '../echarts/index'
 import { Codemirror } from 'vue-codemirror'
 import type { SfcInfo } from '@vue-sfc-online/shared'
 import { vue } from '@codemirror/lang-vue'
-import { Link, Node } from '../types';
+import { Link, Node } from '../types'
 import Demo from './Demo/index.vue'
 
 const dataJson = await fetch('sfc.json')
@@ -12,69 +12,63 @@ const sfcInfo: SfcInfo = await dataJson.json()
 const code = ref(sfcInfo['App.vue']?.__content__ ?? '')
 
 onMounted(() => {
-  const nodes: Node[] = []
-  const links: Link[] = []
-  for(const [name, val] of Object.entries(sfcInfo)) {
-    nodes.push({
-      name,
-      value: val.__path__
-    })
-    for (const key of Object.keys(val)) {
-      if (key !== '__path__') {
-        links.push({ source: name, target: key })
-      }
-    }
-  }
-  const options: any = {
-    series: {
-      type: 'graph',
-      layout: 'force',
-      nodes,
-      links,
-      categories: [
-        { name: '依赖' },
-        { name: '项目依赖' },
-        { name: '项目名' },
-      ],
-      cursor: 'pointer',
-      symbolSize: 22,
-      label: {
-        show: true,
-        position: 'top',
-      },
-      force: {
-        repulsion: 900,
-        friction: 0.15,
-      },
-      roam: true,
-      draggable: true,
-    },
-  }
-  const chart = echarts.init(document.getElementById('chart'))
-  chart.setOption(options)
-  chart.on('click', (params: any) => {
-    const { data } = params
-    code.value = sfcInfo[data.name].__content__
-  })
+	const nodes: Node[] = []
+	const links: Link[] = []
+	for (const [name, val] of Object.entries(sfcInfo)) {
+		nodes.push({
+			name,
+			value: val.__path__,
+		})
+		for (const key of Object.keys(val)) {
+			if (key !== '__path__') {
+				links.push({ source: name, target: key })
+			}
+		}
+	}
+	const options: any = {
+		series: {
+			type: 'graph',
+			layout: 'force',
+			nodes,
+			links,
+			categories: [{ name: '依赖' }, { name: '项目依赖' }, { name: '项目名' }],
+			cursor: 'pointer',
+			symbolSize: 22,
+			label: {
+				show: true,
+				position: 'top',
+			},
+			force: {
+				repulsion: 900,
+				friction: 0.15,
+			},
+			roam: true,
+			draggable: true,
+		},
+	}
+	const chart = echarts.init(document.getElementById('chart'))
+	chart.setOption(options)
+	chart.on('click', (params: any) => {
+		const { data } = params
+		code.value = sfcInfo[data.name].__content__
+	})
 })
 </script>
 
 <template>
-  <div style="display: flex;">
-    <div id="chart" style="height: 100vh;width: 50%;" />
-    <codemirror
-      v-model="code"
-      placeholder="Code goes here..."
-      :style="{ height: '100vh', flex: 1 }"
-      :autofocus="true"
-      :indent-with-tab="true"
-      :tab-size="2"
-      :extensions="[vue()]"
-    />
-  </div>
-  <Demo />
+	<div style="display: flex">
+		<div id="chart" style="height: 100vh; width: 50%" />
+		<codemirror
+			v-model="code"
+			placeholder="Code goes here..."
+			:style="{ height: '100vh', flex: 1 }"
+			:autofocus="true"
+			:indent-with-tab="true"
+			:tab-size="2"
+			:extensions="[vue()]"
+		/>
+	</div>
+	<Demo />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
